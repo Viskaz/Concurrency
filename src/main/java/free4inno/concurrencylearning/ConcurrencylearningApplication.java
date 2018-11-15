deb3798e37930f10324da25071e446cdd15f9e00
viskaz <viskazbu122@163.com>
2018-11-15 21:08:06 +0800

    第一次提交

---
+++ src/main/java/free4inno/concurrencylearning/ConcurrencylearningApplication.java
@@ -0,0 +1,12 @@
+package free4inno.concurrencylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencylearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencylearningApplication.class, args);
+    }
+}
