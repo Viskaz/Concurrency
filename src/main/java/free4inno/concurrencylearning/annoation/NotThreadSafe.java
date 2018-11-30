af36deaac7595ad55b6586554b076973142a0102
viskaz <viskazbu122@163.com>
2018-11-30 16:32:13 +0800

    <ADD> 添加四个注解

---
+++ src/main/java/free4inno/concurrencylearning/annoation/NotThreadSafe.java
@@ -0,0 +1,8 @@
+package free4inno.concurrencylearning.annoation;
+
+
+/**
+ * 线程不安全的
+ */
+public @interface NotThreadSafe {
+}
---
+++ src/main/java/free4inno/concurrencylearning/annoation/ThreadSafe.java
@@ -0,0 +1,16 @@
+package free4inno.concurrencylearning.annoation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标记【线程安全】的类或者写法
+ */
+@Target(ElementType.TYPE) //用于标识线程安全的类
+@Retention(RetentionPolicy.SOURCE) //标识的范围
+public @interface ThreadSafe {
+    String value() default "";
+}
